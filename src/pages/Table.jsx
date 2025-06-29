8438f71d616bb846f898469aa86f9e963ba57def
horstandr <andras.schiphorst@icloud.com>
2025-06-29 13:47:41 +0200

    Update: - Added basic admin panel - Added Order screen - Added components for Button and edited GoBackButton - Added login panel.

--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router";
+
 import Info from '../data/Info';
 import OrdersList from "../components/OrdersList";
 import GoBackButton from "../components/GoBackButton";
+import Button from '../components/Button';
+
+import '../styles/Table.css';
 
 function Table() {
     let params = useParams();
@@ -12,10 +16,11 @@ function Table() {
     return (
         <>
         <GoBackButton />
-        <div className="table">
+        <div className="table-page">
             <h1>{table.name}</h1>
             <OrdersList orders={table.orders} />
             <h4>Total: €{table.totalPrice}</h4>
+            <Button to="/order">New Order</Button>
         </div>
         </>
     );
